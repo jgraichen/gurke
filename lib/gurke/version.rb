d572d8595d6815a51a6e092d096869bcf52d1f7f
Jan Graichen <jg@altimos.de>
2014-06-24 16:32:08 +0200

    Fix fix for ARGV dup

--- lib/gurke/version.rb
+++ lib/gurke/version.rb
@@ -2,7 +2,7 @@ module Gurke
   module VERSION
     MAJOR = 2
     MINOR = 0
-    PATCH = 2
+    PATCH = 3
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
 
