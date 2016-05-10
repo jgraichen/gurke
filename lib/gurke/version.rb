6fe575666e4244452b7ed22eedd1f2c93eab22ec
Malte Swart <mswart@devtation.de>
2016-05-10 14:27:18 +0200

    Release 2.4.2

--- lib/gurke/version.rb
+++ lib/gurke/version.rb
@@ -2,7 +2,7 @@ module Gurke
   module VERSION
     MAJOR = 2
     MINOR = 4
-    PATCH = 1
+    PATCH = 2
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
 
