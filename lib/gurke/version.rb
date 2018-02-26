e4c6d5d02e023c69ba066572293ef0a55a117120
Jan Graichen <jgraichen@altimos.de>
2018-02-26 11:32:54 +0100

    Release 3.0.0

--- lib/gurke/version.rb
+++ lib/gurke/version.rb
@@ -2,9 +2,9 @@
 
 module Gurke
   module VERSION
-    MAJOR = 2
-    MINOR = 4
-    PATCH = 2
+    MAJOR = 3
+    MINOR = 0
+    PATCH = 0
     STAGE = nil
     STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
 
